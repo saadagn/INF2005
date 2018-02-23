7cd3ddc32cd2fcf300701d48a9987ba67000152c
saadagn <saadagn@gmail.com>
2018-02-23 00:33:17 -0500

    tp1

---
+++ header.php
@@ -0,0 +1,8 @@
+<!--affichage du logo-->
+<div class="logo">
+  <img src="images/logo.png" alt="Logo du resto" style="width:500px;height:100px;">
+</div>
+
+<div class="titre de bienvenue">
+  <h1>Bienvenue au restaurant sushi++</h1>
+</div>
