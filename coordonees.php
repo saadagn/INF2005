5a468d99c0ffa1579e4d75abde259b6eb3ef2f31
saadagn <saadagn@gmail.com>
2018-02-23 00:34:41 -0500

    tp1

---
+++ coordonees.php
@@ -0,0 +1,6 @@
+<!--affichage des coordonées-->
+<h3 id="ici" class="coord">Nos coordonnées: </h3>
+<div class="coordonnées">
+  <p><strong>Adresse :</strong> 2020 rue Futomaki, Montréal (QC) J1J K2K</p>
+  <p><strong>Téléphone :</strong> 514-123-4567</p>
+</div>
