cb4964fbc63f33c4ab510ab9b3d90de0c3d2d612
saadagn <saadagn@gmail.com>
2018-02-23 00:29:15 -0500

    first commit du tp1

---
+++ accueil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Site web du restaurant</title>
+  <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+  <?php include("header.php"); ?>
+
+  <?php include("menuChoix.php"); ?>
+
+  <br>
+  <div class="paraAccueil">
+    <p>Je vous souhaite la bienvenue au restaurant Sushi++.</p>
+    <p>Vous êtes présentement connécter à notre site web,</p>
+    <p>ce qui vous permettra de consulter notre choix de menu ainsi que d'effectuer votre commande.</p>
+    <p>Nous serons ravie de vous servir, merci pour votre visite et bonne appetie.</p>
+  </div>
+  <br>
+
+  <?php include("coordonees.php"); ?>
+
+</body>
+</html>
